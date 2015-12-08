17b87b18fe8fe816c156c9b47dcfce529976263d
xCristinaS <cristina.sola.1992@gmail.com>
2015-12-08 14:02:56 +0100

    El 7 hecho con array de objetos

--- Boletin03/src/ejercicio12/Ejercicio12.java
+++ Boletin03/src/ejercicio12/Ejercicio12.java
@@ -1,10 +1,36 @@
 package ejercicio12;
 
+import java.util.Iterator;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import clasesHibernate.*;
+
 public class Ejercicio12 {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Mostrar los datos de todos los profesores junto con los datos de los cursos donde son tutores.
+		// Si no son tutores, indicarlo.
+		SessionFactory sesionFactory = SessionFactoryUtil.getSessionFactory();
+		Session sesion = sesionFactory.openSession();
+		String select = "from Profesor p left join p.cursos";
+		Query cons = sesion.createQuery(select);
+		Iterator<Object> it = cons.iterate();
+		Object[] conjunto;
+		Profesor p; Curso c; 
+		while (it.hasNext()){
+			conjunto = (Object[])it.next();
+			p = (Profesor)conjunto[0];
+			c = (Curso)conjunto[1];
+			if (c != null)
+				System.out.printf("- CodProf: %s | Nombre: %s | Fecha de nacimiento: %s | Alta: %s | Tutor del curso: %s %s\n", p.getCodProf(), p.getNombre(), p.getFechaDeNacimiento(), p.getAlta(), c.getId().getCodCurso(), c.getId().getCodOe());
+			else 
+				System.out.printf("- CodProf: %s | Nombre: %s | Fecha de nacimiento: %s | Alta: %s -- %s\n", p.getCodProf(), p.getNombre(), p.getFechaDeNacimiento(), p.getAlta(), "No es tutor");
+		}
+		sesion.close();
 	}
 
 }
